aaec80d0e79704b610740c511115655f385c0494
leesh235 <wksehs23@naver.com>
2021-03-21 18:53:46 +0900

    second commit

--- Baekjoon_sort/sort_2751.cpp
+++ Baekjoon_sort/sort_2751.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main(void){
 	
-	int n = 0;
-	scanf("%d", &n);
+	int n = 0, a = 0;
+	vector<int> arr;
 	
-	int arr[n] = {0, };
-	
-	//input array 
-	for(int i = 0; i < n; i++{
-		scanf("%d", &arr[i]);
+	//input array
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		cin >> a;
+		arr.push_back(a);
 	}
 	
 	//made heap
-	for(int i = 1; i < n; i++{
+	for(int i = 1; i < arr.size(); i++){
 		int c = i;
 		do{
 			int root = (c - 1) / 2;
@@ -25,6 +26,31 @@ int main(void){
 			}
 			c = root;
 		}while(c != 0);
+	}
+	
+	//sort
+	for(int i = arr.size() - 1; i >= 0; i--){
+		int temp = arr[i];
+		arr[i] = arr[0];
+		arr[0] = temp;
+		int root = 0, c = 1;
+		
+		do{
+			c = 2 * root + 1;
+			if(arr[c] < arr[c + 1] && c < i - 1){
+				c++;
+			}
+			if(arr[c] > arr[root] && c < i){
+				int temp = arr[root];
+				arr[root] = arr[c];
+				arr[c] = temp;
+			}
+			root = c;
+		}while(c < i);
+	}
+	
+	for(int i = 0; i < arr.size(); i++){
+		cout << arr[i] << '\n';
 	}
 	
 	return 0;
